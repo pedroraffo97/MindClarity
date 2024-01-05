5a2e6adec4092278a46300996f96f05211536caa
Alonso Raffo <alonsoraffo97@icloud.com>
2024-01-05 12:35:39 -0500

    Initial Commit

---
+++ MindClarity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MindClarity
+//
+//  Created by Pedro Raffo Leon on 05.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MindClarity/MindClarityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindClarityApp.swift
+//  MindClarity
+//
+//  Created by Pedro Raffo Leon on 05.01.24.
+//
+
+import SwiftUI
+
+@main
+struct MindClarityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
